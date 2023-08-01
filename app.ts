e32169b1baed312a14247e303de54061381f27b8
Kiran S Baliga <60563196+kiransbaliga@users.noreply.github.com>
2023-08-01 18:25:55 +0530

    TS init

--- app.ts
+++ app.ts
@@ -1,12 +1,19 @@
-const express = require("express");
+import express from "express";
 const server = express();
 
 server.get("/", (req, res) => {
   console.log(req.url);
+  const k = "hello";
   res.status(200).send("Hello World Express");
 });
 
 server.listen(3000, () => {
   console.log("server up at http://localhost:3000/");
 });
+
+
+
+
+
+
 
