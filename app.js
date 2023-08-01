293c3db2d053ab06e6ea6c37bf8ef8bf22131ab2
Kiran S Baliga <60563196+kiransbaliga@users.noreply.github.com>
2023-08-01 14:28:11 +0530

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  res.writeHead(200);
+  res.end("Hello World");
+});
+
+server.listen(3000, () => {
+  console.log("server up at http://localhost:3000/");
+});
+
